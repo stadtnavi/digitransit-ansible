e0d36ad1c0d10b86b60fa5041a3be3597c5f2e0c
Leonard Ehrenfried <mail@leonard.io>
2020-02-18 12:04:42 +0100

    Add strong encryption key to OTP container

--- roles/base/templates/alias.sh
+++ roles/base/templates/alias.sh
@@ -1,3 +1,4 @@
 alias ll='ls -lah --color'
 alias digitransit-logs='sudo journalctl -u digitransit-docker-compose -f'
+alias digitransit-logs='sudo journalctl -u data-builder -f'
 alias digitransit-docker-compose='docker-compose -f /etc/digitransit/docker-compose.yml'

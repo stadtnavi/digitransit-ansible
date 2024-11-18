2f0a035f9ac0da3150fbd468f5f319e4ed288a07
Leonard Ehrenfried <mail@leonard.io>
2024-11-18 15:37:12 +0100

    Use proper Lua parser

--- roles/poi/files/themes/stadtnavi/topics/pois.lua
+++ roles/poi/files/themes/stadtnavi/topics/pois.lua
@@ -9,6 +9,8 @@ local themepark, theme, cfg = ...
 
 local parser = require 'themepark/parser'
 
+local csv = require('lua-csv/csv')
+
 themepark:add_table{
     name = 'pois',
     ids_type = 'any',
@@ -31,33 +33,6 @@ themepark:add_table{
     }),
 }
 
--- taken from https://www.lua.org/pil/20.4.html
-local function from_csv(s)
-    s = s .. ','        -- ending comma
-    local t = {}        -- table to collect fields
-    local fieldstart = 1
-    repeat
-    -- next field is quoted? (start with `"'?)
-    if string.find(s, '^"', fieldstart) then
-        local a, c
-        local i  = fieldstart
-        repeat
-            -- find closing quote
-            a, i, c = string.find(s, '"("?)', i+1)
-        until c ~= '"'    -- quote not followed by quote?
-        if not i then error('unmatched "') end
-        local f = string.sub(s, fieldstart+1, i-1)
-        table.insert(t, (string.gsub(f, '""', '"')))
-        fieldstart = string.find(s, ',', i) + 1
-    else                -- unquoted; find next comma
-        local nexti = string.find(s, ',', fieldstart)
-        table.insert(t, string.sub(s, fieldstart, nexti-1))
-        fieldstart = nexti + 1
-    end
-    until fieldstart > string.len(s)
-    return t
-end
-
 local map_columns = {
     Oberkategorie = 'category1',
     Kategorie = 'category2',
@@ -68,19 +43,17 @@ local map_columns = {
 
 local function read_config(filename)
     local config = {}
-    local columns = {}
 
-    for line in io.lines(filename) do
-        local d = from_csv(line)
-        if #columns == 0 then
-            columns = d
-        else
-            local l = {}
-            for idx, c in ipairs(d) do
-                l[map_columns[columns[idx]] or columns[idx]] = c
-            end
-            config[#config + 1] = l
+    local f = csv.open(filename, {
+        header = true,
+        separator = ',',
+    })
+    for fields in f:lines() do
+        local cfg = {}
+        for k, v in pairs(map_columns) do
+            cfg[v] = fields[k]
         end
+        config[#config + 1] = cfg
     end
 
     return config
